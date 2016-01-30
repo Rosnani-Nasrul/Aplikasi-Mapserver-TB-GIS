70d4fe8d4fc29d5999bcff52c0c6739e58d64788
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 03:35:59 +0700

    menambahkan deskripsi npm pd nama nur fatimah

--- webs/About.php
+++ webs/About.php
@@ -17,7 +17,7 @@
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Kita dari kelompok tugas besar GIS yang beranggotakan 4 orang mahasiswi yaitu:</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">1. Rosnani Nasrul dengan npm 1134015</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari dengan npm 1134015</font></p>
-	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan</font></p>
+	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan dengan npm 1134016</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">4. Hariyati Suarni Nurindah</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Semoga aplikasi web ini memberikan informasi yang bermanfaat bagi para pengguna dalam mengetahui peta lokasi pada web ini.</font></p>
