922ce48f8568bc3097df17026078b407830397e2
Rosnani Nasrul <rosnaninasrul.rn@gmail.com>
2016-01-31 02:47:13 +0700

    Menambahkan kurung kurawal yang kurang

--- webs/logout.php
+++ webs/logout.php
@@ -1,6 +1,8 @@
 <? session_start();
 if (session_is_registered('user_id'))
+{
 //hapus session
 session_unregister ("user_id");
 session_unregister ("user_name");
 echo "<script> document.location.href='index.php'; </script>";
+}
