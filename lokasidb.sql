bcf74c819ae9a3a3b74bcf0e2b15c9f476eec284
Hariyati Suarni Nurindah <hariyatisuarni@gmail.com>
2016-01-31 02:05:01 +0700

    Edit Id Table

--- lokasidb.sql
+++ lokasidb.sql
@@ -25,6 +25,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
- `id` int(3) NOT NUL AUTO_INCREMENT,
+ `id` int(3) NOT NULL AUTO_INCREMENT,
  `nama` int(255) COLLATE latin1_genral_ci NOT NULL,
  `email` varchar(255) COLLATE latin1_general_ci NOT NULL,
