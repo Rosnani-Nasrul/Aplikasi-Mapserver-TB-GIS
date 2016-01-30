f4e4708e6b002be7d9eac05548767cb9ab069316
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 02:17:59 +0700

    next query untuk tampilan tulisan selamat datang

--- webs/welcome.php
+++ webs/welcome.php
@@ -7,3 +7,8 @@
 	<blockquote>
 	<font face="Arial, Helvetica, sans-serif" color="#000000" size="2"><h4>
 	  <script language="JavaScript" type="text/javascript">
+	  <!--
+		var message="Selamat Datang..."
+		var neonbasecolor="white"
+		var neontextcolor="black"
+		var flashspeed=100  //dalam milisekon
