225bf5379109a1d0968f748e258017d311d01822
Rosnani Nasrul <rosnaninasrul.rn@gmail.com>
2016-01-31 01:54:36 +0700

    Modifikasi pertama lokasidb.sql

--- lokasidb.sql
+++ lokasidb.sql
@@ -1,1 +1,16 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 09, 2011 at 09:48 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
 
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
