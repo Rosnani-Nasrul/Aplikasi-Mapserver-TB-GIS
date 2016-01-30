fc52693e6461fd8c41f6c6541768533d86af57fd
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 03:34:11 +0700

    menambahkan deskripsi npm pd nama nur mulyana

--- webs/About.php
+++ webs/About.php
@@ -16,7 +16,7 @@
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Hii Teman-Teman ...</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Kita dari kelompok tugas besar GIS yang beranggotakan 4 orang mahasiswi yaitu:</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">1. Rosnani Nasrul dengan npm 1134015</font></p>
-	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari</font></p>
+	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari dengan npm 1134015</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">4. Hariyati Suarni Nurindah</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B</font></p>
