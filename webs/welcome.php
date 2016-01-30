ca8aa4b4536d117a9c8b9318426b083b72320a75
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 02:16:25 +0700

    next query untuk tampilan welcome

--- webs/welcome.php
+++ webs/welcome.php
@@ -1,3 +1,9 @@
 <html>
 <head><title>Welcome to Web Gis Menampilkan Peta Lokasi</title></head>
 <body>
+<table>
+<tr>
+	<td>
+	<blockquote>
+	<font face="Arial, Helvetica, sans-serif" color="#000000" size="2"><h4>
+	  <script language="JavaScript" type="text/javascript">
