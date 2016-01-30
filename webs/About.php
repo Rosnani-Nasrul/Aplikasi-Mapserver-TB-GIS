093cbdd3992cd535b5272c38e20752d6310167ff
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 02:40:28 +0700

    query letak posisi tabel about

--- webs/About.php
+++ webs/About.php
@@ -4,4 +4,7 @@
 <title>Web GIS Menampilkan Peta Lokasi</title>
 <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
 </head>
-<body></body>
+<body>
+  <center>
+<table width="100%" cellspacing="2" cellpadding="2" >
+</body>
