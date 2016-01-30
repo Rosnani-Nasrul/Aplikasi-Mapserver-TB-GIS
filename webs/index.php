3fce2f7b9d78d13bc964bd9526554739b3b7dc42
Rosnani Nasrul <rosnaninasrul.rn@gmail.com>
2016-01-31 02:07:13 +0700

    Modifikasi ketujuh index.php

--- webs/index.php
+++ webs/index.php
@@ -3,8 +3,9 @@
     <title>TB GIS MAPSERVER</title>
   </head>
   <body>
-    <td valign="top" bgcolor="#9ABC67" >
+    <td valign="top" bgcolor="#9ABC67" ><div align="center">
       
+    </div
     </td>
   </body>
 </body>
