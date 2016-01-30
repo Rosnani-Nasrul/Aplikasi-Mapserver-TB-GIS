87e786ac500aeb580c1b3e15127976b00956ab20
Hariyati Suarni Nurindah <hariyatisuarni@gmail.com>
2016-01-31 02:21:22 +0700

    Edit nama pada create

--- lokasidb.sql
+++ lokasidb.sql
@@ -26,7 +26,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `admin` (
  `id` int(3) NOT NULL AUTO_INCREMENT,
- `nama` Varchar(255) COLLATE latin1_genral_ci NOT NULL,
+ `nama` Varchar(255) COLLATE latin1_general_ci NOT NULL,
  `email` varchar(255) COLLATE latin1_general_ci NOT NULL,
 `kelamin` varchar(8) COLLATE latin1_geeral_ci NOT NULL,
 `user` int(25) COLLATE latin1_general_ci NOT NULL,
