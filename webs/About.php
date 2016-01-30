7156e1799f1c78b4702bdc62dd57039c0eda47cd
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 03:02:52 +0700

    query tampilan posisi dan size deskripsi ketujuh

--- webs/About.php
+++ webs/About.php
@@ -19,4 +19,5 @@
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">4. Hariyati Suarni Nurindah</font></p>
+	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">
 </body>
