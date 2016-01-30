880776dde34c512264c068b8dee446975af7b31c
Rosnani Nasrul <rosnaninasrul.rn@gmail.com>
2016-01-31 02:08:36 +0700

    Modifikasi kesembilan index.php

--- webs/index.php
+++ webs/index.php
@@ -5,6 +5,7 @@
   <body>
     <td valign="top" bgcolor="#9ABC67" ><div align="center">
       <p><font face="comic sans" size="8" color="#000000">Menampilkan Peta Lokasi </font> </p>
+    <p>&nbsp;</p>
     </div
     </td>
   </body>
