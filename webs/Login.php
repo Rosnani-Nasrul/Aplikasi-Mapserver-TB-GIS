e63a37f6d8a42255f62a32e9108de007042d9086
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 02:02:23 +0700

    tambahan query tampilan untuk username

--- webs/Login.php
+++ webs/Login.php
@@ -70,3 +70,7 @@ else
 		    
 		<form name="masuk" action="login.php" method="post">
 		<table border="0" bordercolor="blue" align="left">
+	<tr>	
+		<td><font face="verdana" size="2">Username</font></td>
+		<td><input type="text" size="10" name="username" id="userid"></td>
+	</tr>
