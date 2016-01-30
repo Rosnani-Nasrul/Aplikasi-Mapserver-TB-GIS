b8513ef2b14c31221b7ecd9aab7894f916311fa7
Rosnani Nasrul <rosnaninasrul.rn@gmail.com>
2016-01-31 02:34:37 +0700

    Update lokasidb.sql

--- lokasidb.sql
+++ lokasidb.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 09, 2011 at 09:48 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
