e883f87c25ab6852a46c9f91627eb0368c97dec3
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 04:12:11 +0700

    Update About.php

--- webs/About.php
+++ webs/About.php
@@ -16,7 +16,7 @@
 	<p align="justify"><font size="2" face="Times New Roman">Hii Teman Semua...</font></p>
 	<p align="justify"><font size="2" face="Times New Roman">Kita dari kelompok tugas besar GIS yang beranggotakan 4 orang mahasiswi yaitu:</font></p>
 	<p align="justify"><font size="2" face="Times New Roman">1. Rosnani Nasrul dengan npm 1134015</font></p>
-	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari dengan npm 1134047</font></p>
+	<p align="justify"><font size="2" face="Times New Roman">2. Nur Mulyana Mustari dengan npm 1134047</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan dengan npm 1134016</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">4. Hariyati Suarni Nurindah dengan npm 1134021</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B dan kami berasaln dari daerah provinsi yang sama yaitu sulawesi selatan</font></p>
