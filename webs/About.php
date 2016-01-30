8f62f2822c2d31e7de41c1230f830edc87b17aa4
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 03:04:22 +0700

    penutup deskripsi ketujuh

--- webs/About.php
+++ webs/About.php
@@ -19,5 +19,5 @@
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">3. Nur Fatimah Hasan</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">4. Hariyati Suarni Nurindah</font></p>
-	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B
+	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B</font></p>
 </body>
