638b622fcdca6377360214374cd3624264eadbd1
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 04:10:53 +0700

    edit jenis tulisan

--- webs/About.php
+++ webs/About.php
@@ -13,7 +13,7 @@
 </td>
 <tr>
 	<td valign="top"><p>&nbsp;</p>
-	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Hii Teman Semua...</font></p>
+	<p align="justify"><font size="2" face="Times New Roman">Hii Teman Semua...</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Kita dari kelompok tugas besar GIS yang beranggotakan 4 orang mahasiswi yaitu:</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">1. Rosnani Nasrul dengan npm 1134015</font></p>
 	<p align="justify"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">2. Nur Mulyana Mustari dengan npm 1134047</font></p>
