85fd58f8cfd40e1ed0ebb77f2db6d7c2ab290359
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 02:42:54 +0700

    query pemberian warna dan jenis tulisan pd About Us

--- webs/About.php
+++ webs/About.php
@@ -9,4 +9,5 @@
 <table width="100%" cellspacing="2" cellpadding="2" >
 <tr>
 <td bgcolor="#0000FF" colspan="2"><div align="center">
+  <font color="#FFFFFF" face="Geneva, Arial, Helvetica, sans-serif">
 </body>
