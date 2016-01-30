9cb45fe7e0f7ceeea27b50a6b5805c0de5dd317c
Hariyati Suarni Nurindah <hariyatisuarni@gmail.com>
2016-01-31 02:42:40 +0700

    sessoin pembatas

--- webs/logout.php
+++ webs/logout.php
@@ -1,5 +1,5 @@
 <? session_start();
 if (session_is_registered('user_id'))
 //hapus session
-session_unregister ("user_i")
-session_unregister ("user_name")
+session_unregister ("user_i");
+session_unregister ("user_name");
