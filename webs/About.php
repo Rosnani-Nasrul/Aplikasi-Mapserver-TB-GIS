1415a8b1f0b0477bfe37d91e0cbfc5f72174bb46
Nur Fatimah Hasan <Nurfatimahhasan18@gmail.com>
2016-01-31 04:14:28 +0700

    hapus jenis tulisan sans-serif

--- webs/About.php
+++ webs/About.php
@@ -19,7 +19,7 @@
 	<p align="justify"><font size="2" face="Times New Roman">2. Nur Mulyana Mustari dengan npm 1134047</font></p>
 	<p align="justify"><font size="2" face="Times New Roman">3. Nur Fatimah Hasan dengan npm 1134016</font></p>
 	<p align="justify"><font size="2" face="Times New Roman">4. Hariyati Suarni Nurindah dengan npm 1134021</font></p>
-	<p align="justify"><font size="2" face="Times New Roman, sans-serif">Dari jurusan D4 Teknik Informatika kelas 3B dan kami berasaln dari daerah provinsi yang sama yaitu sulawesi selatan</font></p>
+	<p align="justify"><font size="2" face="Times New Roman">Dari jurusan D4 Teknik Informatika kelas 3B dan kami berasaln dari daerah provinsi yang sama yaitu sulawesi selatan</font></p>
 	<p align="justify"><font size="2" face="Times New Roman">Kami Berharap Semoga aplikasi web ini memberikan informasi yang bermanfaat bagi para pengguna dalam mengetahui peta lokasi pada web ini.</font></p>
 	<p align="Center"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Terima Kasih Kunjungannya</font></p>
 	</td>
